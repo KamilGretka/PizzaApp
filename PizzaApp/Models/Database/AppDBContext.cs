35898ac0b410c8d7b62cd7a72b30f02f6e0c11e0
Kamil Gretka <infermus96@gmail.com>
2020-10-08 13:19:05 +0200

    fixed problem connection string path

--- PizzaApp/Models/Database/AppDBContext.cs
+++ PizzaApp/Models/Database/AppDBContext.cs
@@ -1,10 +1,13 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
+using System.IO;
 
 namespace PizzaApp.Models.Database
 {
     public class AppDBContext : DbContext
     {
-        public AppDBContext() : base("name=DefaultConnection")
+        public AppDBContext() : base(string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={0}\PizzaApp\Database\PizzaAppDB.mdf;Integrated Security=True",
+                                                Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName))
         {
 
         }
